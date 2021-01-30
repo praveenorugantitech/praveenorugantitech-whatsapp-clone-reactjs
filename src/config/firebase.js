2fa117800b9b20d00cd04d52ddf2d67abae80391
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-31 01:48:34 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-import firebase from "firebase";
 
-
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
   appId:process.env.REACT_APP_FIREBASE_APP_ID,
-};
+});
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
 const database = firebaseApp.firestore();
 const authentication = firebaseApp.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
